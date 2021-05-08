77c1b984a8504fa229e6a3e5513a540af74f6703
Ali <alibhutto64@outlook.com>
2021-05-09 01:12:08 +0800

    new page: Surah

--- components/AddAyah.js
+++ components/AddAyah.js
@@ -5,6 +5,7 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import Button from '@material-ui/core/Button';
+import React from 'react';
 
 const useStyles = makeStyles((theme) => ({
     root: {
---
+++ pages/islamic/surah.js
@@ -0,0 +1,61 @@
+import { makeStyles, Typography } from "@material-ui/core";
+import React from "react";
+
+const UseStyles = makeStyles(()=>({
+    root: {
+        marginTop: '50px'
+    },
+    header: {
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        alignContent: 'center',
+        width: '70vw',
+        margin: '0 auto',
+        flexWrap: 'wrap'
+    },
+    title: {
+        borderTop: 'solid #EDEAF9',
+        borderLeft: 'solid #EDEAF9',
+        padding: '15px'
+    },
+    basic: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        width: '300px',
+        backgroundColor: '#FCFBFF',
+        border: 'solid #EDEAF9',
+        padding: '20px',
+        borderRadius: '10px'
+
+    }
+}))
+
+export default function surah() {
+    const classes = UseStyles()
+    return (
+        <div className={classes.root}>
+            <header className={classes.header}>
+                <div className={classes.title}>
+                    <Typography variant="h3">Surah Al-Hijr</Typography>
+                    <Typography variant="h4" >(The Rock)</Typography>
+                </div>
+
+                <div className={classes.basic}>
+                    <div>
+                        <Typography variant="h5">15</Typography>
+                        <Typography variant="subtitle1">Surah</Typography>
+                    </div>
+                    <div>
+                        <Typography variant="h5">99</Typography>
+                        <Typography variant="subtitle1">Ayah</Typography>
+                    </div>
+                    <div>
+                        <Typography variant="h5">14</Typography>
+                        <Typography variant="subtitle1">Juz</Typography>
+                    </div>
+                </div>
+            </header>
+        </div>
+    )
+}
